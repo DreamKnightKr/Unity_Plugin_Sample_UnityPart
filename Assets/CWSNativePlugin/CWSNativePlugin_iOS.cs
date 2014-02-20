49f7c6113e63a8ca16cb3ecd5b1cc1088d605b73
DreamKnightMac <dreamknightkr@gmail.com>
2014-02-21 05:10:28 +0900

    iOS Web View 기능 추가.

--- Assets/CWSNativePlugin/CWSNativePlugin_iOS.cs
+++ Assets/CWSNativePlugin/CWSNativePlugin_iOS.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class CWSNativePlugin_iOS : CWSNativePlugin_Dummy {
 #if UNITY_IPHONE
+	GameObject obj = null;
+
 	public override void Init() {}
 	public override string GetName()
 	{
@@ -12,6 +14,34 @@ public class CWSNativePlugin_iOS : CWSNativePlugin_Dummy {
 	public override string GetVersion()
 	{
 		return "v1.0.0";
+	}
+
+	public override void CallSimpleFunction (string name)
+	{
+		if (null != obj)
+			return;
+
+		obj = new GameObject ("WebViewiOS");
+		WebViewBehavior webview = obj.AddComponent<WebViewBehavior>();
+
+		if (webview != null)
+			webview.SimpleFunction (name, "OnCalledSimpleFunction", "Native Return SomeString...");
+	}
+
+	public override void OpenEmbeddedWebPage(string url)
+	{
+		if (null != obj)
+			return;
+		
+		obj = new GameObject ("WebViewiOS");
+		WebViewBehavior webview = obj.AddComponent<WebViewBehavior>();
+
+		if( webview != null )
+		{
+			webview.SetMargins(10, 400, 10, 10);
+			webview.LoadURL( "https://www.google.co.jp" );
+			webview.SetVisibility( true );
+		}
 	}
 #endif
 }
